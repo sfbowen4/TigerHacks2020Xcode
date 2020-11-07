da09dc889e6849be30de09e1e25a8782d6284057
AJ Wilkinson <39915791+AJWilkinson12@users.noreply.github.com>
2020-11-06 23:56:05 -0600

    Created user object

---
+++ AdventureMap/AdventureMap/user.swift
@@ -0,0 +1,18 @@
+//
+//  user.swift
+//  AdventureMap
+//
+//  Created by AJ Wilkinson on 11/6/20.
+//
+
+import Foundation
+
+class User{
+    
+    var userName:String = ""
+    var password:String = ""
+    var points:Int = 0
+    var adventures:Int = 0
+    var completed:Int = 0
+    
+}
