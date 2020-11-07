c367e3aa79ed61b1c8265fb5d3eb88fdf34143fa
AJ Wilkinson <39915791+AJWilkinson12@users.noreply.github.com>
2020-11-07 08:43:05 -0600

    Uploaded base sorting of POI and interest list creation.

---
+++ AdventureMap/AdventureMap/createInterestList.swift
@@ -0,0 +1,18 @@
+//
+//  createInterestList.swift
+//  AdventureMap
+//
+//  Created by AJ Wilkinson on 11/7/20.
+//
+
+import Foundation
+
+var interestList = [Int]()
+
+/*
+while searching{
+    if POI selected{
+        interestList.append(selectedPOIhash)
+    }
+ }
+ */
