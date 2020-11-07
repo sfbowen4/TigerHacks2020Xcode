8f0795a266d66a24d2771b827f67d7ab583ea023
Stephen Bowen <Sfbowen4@gmail.com>
2020-11-06 20:45:04 -0600

    Implemented Google Maps Formal

---
+++ AdventureMap/AdventureMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdventureMap
+//
+//  Created by Stephen Bowen on 11/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
