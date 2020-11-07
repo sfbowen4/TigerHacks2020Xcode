2dda4ae21f3004229d4ac13fd94f77213940cffb
Stephen Bowen <62777692+sfbowen4@users.noreply.github.com>
2020-11-06 18:24:55 -0600

    First Commit

---
+++ Adventure Map/Adventure Map/Adventure_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adventure_MapApp.swift
+//  Adventure Map
+//
+//  Created by Stephen Bowen on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Adventure_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
